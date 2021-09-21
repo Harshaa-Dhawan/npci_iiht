d9d4564f9807dca574e5f8cb809aa60662b20893
Harshaa-Dhawan <harshaadhawan1999@gmail.com>
2021-09-21 16:43:48 +0530

    Assignment4

---
+++ Java Assignment/Assignment4.java
@@ -0,0 +1,36 @@
+//Reverse fibonacci series till 100
+
+
+import java.io.*;
+
+class Assignment4 {
+	
+	static void reverseFibonacci()
+	{
+		int a[] = new int[100];
+	
+
+		a[0]=0;
+		a[1]=1;
+	
+		for (int i=2; i<100; i++)
+		{
+	
+			a[i] = a[i-2] + a[i-1];
+		}
+	
+		for (int i=99; i>=0; i--)
+		{
+	
+			System.out.print(a[i] +" ");
+		}
+	}
+	
+	
+	public static void main(String[] args)
+	{
+		
+		reverseFibonacci(100);
+	
+	}
+}
